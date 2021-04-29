890443cbbaad942b390da9052617c90cc29f5d34
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-04-29 20:44:31 +0900

    フィボナッチ数列計算の関数をちゃんと書いた

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,4 +1,14 @@
 pub fn fibonacci(n: i32) -> i32 {
+	fn calc(left: i32, right: i32, m: i32) -> i32 {
+		if m == 0 {
+			return left;
+		} else {
+			return calc(left + right, left, m - 1);
+		}
+	}
+	return calc(1, 0, n);
+}
+pub fn fibonacci_match(n: i32) -> i32 {
 	match n {
 		0 => 0,
 		1 => 1,
