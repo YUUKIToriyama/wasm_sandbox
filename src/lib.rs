16ba33423b0fba13853d4e92d1b4aa3b666b3542
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-04-29 18:27:05 +0900

    draw circle on canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::f64;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use wasm_bindgen::JsCast;
 
 #[wasm_bindgen]
 pub fn say_hello(s: &str) {
@@ -16,4 +18,20 @@ pub fn display() -> Result<(), JsValue> {
     body.append_child(&val)?;
 
     Ok(())
+}
+#[wasm_bindgen]
+pub fn draw() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>().map_err(|_| ()).unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+    context.arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0).unwrap();
+    context.stroke();
 }
